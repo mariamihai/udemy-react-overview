1a8732abcc64225ce2042c9295af290854fadca9
mariamihai <maria.ic.mihai@gmail.com>
2021-04-15 10:00:19 +0200

    Cleaning up App.js

--- src/App.js
+++ src/App.js
@@ -28,29 +28,19 @@ function App() {
     }
   ];
 
+  const listOfExpenses = expenses.map((expense) => (
+    <ExpenseItem
+      title={expense.title}
+      date={expense.date}
+      amount={expense.amount}
+    />
+  ));
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
+
+      {listOfExpenses}
     </div>
   );
 }
