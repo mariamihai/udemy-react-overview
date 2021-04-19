28562bbe78eafe21b418d795ae2afaf3815a6348
mariamihai <maria.ic.mihai@gmail.com>
2021-04-19 14:03:36 +0200

    [Section 4.48] Adding state to ExpenseItem as example of using state

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
 
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("ABC");
+    setTitle("New title");
   };
 
   return (
@@ -13,7 +17,7 @@ function ExpenseItem(props) {
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
