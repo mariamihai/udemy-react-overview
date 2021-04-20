f5c3b8a9f28e2d64f2876f3219aa7cb53fd42699
mariamihai <maria.ic.mihai@gmail.com>
2021-04-20 10:55:59 +0200

    [Section 5.67] Splitting expenses list to different component (Adding conditional return statements.)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Card from "../UI/Card/Card";
 
-import ExpenseItem from "./ExpenseItem/ExpenseItem";
+import ExpensesList from "./ExpensesList/ExpensesList";
 import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 
 import "./Expenses.css";
@@ -19,26 +19,16 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === year;
   });
 
-  var expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length !== 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        date={expense.date}
-        amount={expense.amount}
-      />
-    ));
-  }
-
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={year}
-        onUpdateFilterYear={updateFilterYearHandler}
-      />
-      {expensesContent}
-    </Card>
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={year}
+          onUpdateFilterYear={updateFilterYearHandler}
+        />
+        <ExpensesList filteredExpenses={filteredExpenses} />
+      </Card>
+    </li>
   );
 }
 
