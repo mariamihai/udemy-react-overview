b79b0a18e9e0c610ef72e664c3166d0bd95cc003
mariamihai <maria.ic.mihai@gmail.com>
2021-04-20 12:14:55 +0200

    [Section 5.Assignment 4] Hide and show new expense form (Adding conditional return statements)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,6 +30,16 @@ const ExpenseForm = (props) => {
 
     props.onSaveExpenseData(expenseData);
 
+    resetComponent();
+  };
+
+  const clickCancelHandler = () => {
+    resetComponent();
+
+    props.onClick();
+  };
+
+  const resetComponent = () => {
     setTitle("");
     setAmount("");
     setDate("");
@@ -66,6 +76,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={clickCancelHandler}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,38 @@
+import { useState } from "react";
+
 import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  /* isEditing */
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredData) => {
     const expenseData = {
       ...enteredData,
       id: Math.random().toString()
     };
+    setShowForm(false);
 
     props.onAddExpenseData(expenseData);
-    console.log(expenseData);
+  };
+
+  const showFormHandler = () => {
+    setShowForm((prevState) => {
+      return !prevState;
+    });
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onClick={showFormHandler}
+        />
+      )}
     </div>
   );
 };
