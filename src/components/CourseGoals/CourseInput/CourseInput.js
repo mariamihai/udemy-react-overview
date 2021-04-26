d62b4b79b8c410561dfdd5004e0c549c6f24fe6a
mariamihai <maria.ic.mihai@gmail.com>
2021-04-26 12:59:51 +0200

    [Section 6.74] Check if the input is valid when submitting (Setting dynamic inline styles)

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,34 +6,49 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [value, setValue] = useState("");
-  const [disabled, setDisabled] = useState(true);
+  //const [disabled, setDisabled] = useState(true);
+  const [isValidInput, setIsValidInput] = useState(true);
 
   const inputChangedHandler = (event) => {
     setValue(event.target.value);
 
-    disableAddGoalForInvalidInput(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValidInput(true);
+    }
+    //disableAddGoalForInvalidInput(event.target.value);
   };
 
-  const disableAddGoalForInvalidInput = (text) => {
-    text.length < 3 ? setDisabled(true) : setDisabled(false);
-  };
+  //const disableAddGoalForInvalidInput = (text) => {
+  //  text.trim().length < 3 ? setDisabled(true) : setDisabled(false);
+  //};
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
     props.onAddGoal(value);
+    if (value.trim().length === 0) {
+      setIsValidInput(false);
+    }
     setValue("");
-    setDisabled(true);
+    //setDisabled(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" value={value} onChange={inputChangedHandler} />
+        <label style={{ color: isValidInput ? "black" : "red" }}>
+          Course Goal
+        </label>
+        <input
+          style={{
+            borderColor: isValidInput ? "black" : "red",
+            background: isValidInput ? "transparent" : "salmon"
+          }}
+          type="text"
+          value={value}
+          onChange={inputChangedHandler}
+        />
       </div>
-      <Button type="submit" disabled={disabled}>
-        Add Goal
-      </Button>
+      <Button type="submit" /*disabled={disabled}*/>Add Goal</Button>
     </form>
   );
 };
