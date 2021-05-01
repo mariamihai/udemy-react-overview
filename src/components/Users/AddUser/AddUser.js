6f46cc7734e4207a5458c93429dd00a13135258c
mariamihai <maria.ic.mihai@gmail.com>
2021-05-01 13:41:18 +0200

    [Section 8.92] Adding state for username and age

--- src/components/Users/AddUser/AddUser.js
+++ src/components/Users/AddUser/AddUser.js
@@ -1,21 +1,44 @@
+import { useState } from "react";
+
 import styles from "./AddUser.module.css";
 
 import Card from "../../UI/Card/Card";
 import Button from "../../UI/Button/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+          type="text"
+        />
 
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+          type="number"
+        />
 
         <Button type="submit">Add user</Button>
       </form>
