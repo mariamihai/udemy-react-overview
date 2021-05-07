0ec01c7917d3b1539b4c5ef46c1f66201c0f747e
mariamihai <maria.ic.mihai@gmail.com>
2021-05-07 07:31:29 +0000

    [Section 10.124] Added a custom context provider component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import { useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
+import Button from "../UI/Button/Button";
 
 const Home = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={ctx.onLogout}>Logout</Button>
     </Card>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import { useState, useEffect, useReducer } from "react";
+import { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -29,6 +31,8 @@ const passwordReducer = (state, action) => {
 };
 
 const Login = (props) => {
+  const ctx = useContext(AuthContext);
+
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -73,7 +77,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
