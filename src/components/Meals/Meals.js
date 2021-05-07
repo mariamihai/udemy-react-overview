1f7dc52ecaa77c71ba98033858522d412cb4f470
mariamihai <maria.ic.mihai@gmail.com>
2021-05-07 12:27:36 +0000

    [Section 11.134] Added Meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary/MealsSummary";
+import AvailableSummary from "./AvailableMeals/AvailableMeals";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableSummary />
+    </>
+  );
+};
+
+export default Meals;
