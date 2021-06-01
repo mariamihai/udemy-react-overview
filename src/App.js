e214f33bd30b1a4bafba61ad57c903b33d7debe4
mariamihai <maria.ic.mihai@gmail.com>
2021-06-01 11:35:47 +0000

    [Section 14.177] Handling loading and user messages for different data states

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -19,6 +22,8 @@ function App() {
       };
     });
     setMovies(transformedMovieList);
+
+    setIsLoading(false);
   }
 
   return (
@@ -27,7 +32,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
