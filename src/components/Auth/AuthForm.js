af11ce3df9ceb5caafb44ca3561d16613febe17d
mariamihai <maria.ic.mihai@gmail.com>
2021-09-20 22:14:03 +0200

    [301] Adding user sign up

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,26 +1,69 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import styles from "./AuthForm.module.css";
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // Add validation
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=<API_KEY>",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((response) => {
+        if (response.ok) {
+          // Do smth...
+        } else {
+          return response.json().then((data) => {
+            // show an error modal
+            console.log(data);
+          });
+        }
+      });
+    }
+  };
+
   return (
     <section className={styles.auth}>
       <h1>{isLogin ? "Login" : "Sign up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={styles.control}>
           <label htmlFor="email">Your email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
 
         <div className={styles.control}>
           <label htmlFor="password">Your password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
 
         <div className={styles.actions}>
