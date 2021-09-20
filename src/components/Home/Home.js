ed04eae0a8f3f81439adc5a50bd4077fdcaa50f9
mariamihai <maria.ic.mihai@gmail.com>
2021-09-20 20:46:29 +0200

    Adding the Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <section className={styles.main}>
+      <h1>Welcome on Board!</h1>
+    </section>
+  );
+};
+
+export default Home;
