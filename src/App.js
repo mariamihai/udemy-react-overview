6a86d70116b2344e152d70430468023f15a768c0
mariamihai <maria.ic.mihai@gmail.com>
2021-09-21 09:03:55 +0000

    [308] Protect frontend pages based on authentication status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
@@ -13,12 +18,25 @@ function App() {
           <HomePage />
         </Route>
 
-        <Route path="/auth" exact>
-          <AuthPage />
-        </Route>
+        {!authCtx.isLoggedIn &&
+          <Route path="/auth" exact>
+            <AuthPage />
+          </Route>
+        }
 
-        <Route path="/profile" exact>
-          <ProfilePage />
+        { authCtx.isLoggedIn &&
+          <Route path="/profile" exact>
+            <ProfilePage />
+          </Route>
+        }
+
+        {/* <Route path="/profile" exact>
+            { authCtx.isLoggedIn && <ProfilePage />}
+            { !authCtx.isLoggedIn && <Redirect to='/' />}
+        </Route> */}
+
+        <Route path='*'>
+          <Redirect to='/' />
         </Route>
       </Switch>
     </Layout>
